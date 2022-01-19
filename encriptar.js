4ad06897b84263f198c7618abc49fea5eb4b8d76
Pedro Danjack Ortiz <danjack010892@gmail.com>
2022-01-19 14:48:45 -0500

    Se modifica CSS

--- encriptar.js
+++ encriptar.js
@@ -1,34 +1,40 @@
 const valores = {
-    e: "enter",
-    i: "imes",
-    a: "ai",
-    o: "ober",
-    u: "ufat",
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  o: "ober",
+  u: "ufat",
 };
 
-let textoUsuario = document.querySelector("#msg").value;
-
 let result = document.querySelector("#result");
+let textoUsuario = document.querySelector("#msg");
 
 function encriptar() {
-    let textoEncriptado = "";
-    for (const llave in valores) {
-        textoEncriptado = textoUsuario.replaceAll(llave, valores[llave]);
+  let texto = textoUsuario.value;
+  let textoEncriptado = "";
+  for (const llave in valores) {
+    textoEncriptado = texto.replaceAll(llave, valores[llave]);
 
-        textoUsuario = textoEncriptado;
-    }
+    texto = textoEncriptado;
+  }
 
-    result.value = textoEncriptado;
+  result.value = textoEncriptado;
+  textoUsuario.value = "";
 }
 
 function desencriptar() {
-    let textoDesencriptado = "";
-    for (const llave in valores) {
-        textoDesencriptado = textoUsuario.replaceAll(valores[llave], llave);
+  let textoUsuario = document.querySelector("#msg").value;
+  let textoDesencriptado = "";
+  for (const llave in valores) {
+    textoDesencriptado = textoUsuario.replaceAll(valores[llave], llave);
 
-        textoUsuario = textoDesencriptado;
-    }
-    result.value = textoDesencriptado;
+    textoUsuario = textoDesencriptado;
+  }
+  result.value = textoDesencriptado;
+}
+function copiar() {
+  let textoCopiado = document.querySelector("#result").value;
+  navigator.clipboard.writeText(textoCopiado);
 }
 
 var button1 = document.querySelector("#cryp");
@@ -36,3 +42,6 @@ button1.onclick = encriptar;
 
 var button2 = document.querySelector("#decryp");
 button2.onclick = desencriptar;
+
+var button3 = document.querySelector("#copy");
+button3.onclick = copiar;
